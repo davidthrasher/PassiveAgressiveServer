6e01132f7128f0b24b4f5f925bb1457af4ed54ad
David Thrasher <thrasher1332@hotmail.com>
2017-08-01 19:43:39 -0400

    Random phrases printing to console

--- serverFunctionality.js
+++ serverFunctionality.js
@@ -5,19 +5,25 @@ var http = require("http");
 var PORTONE = 7000;
 var PORTTWO = 7500;
 
+var positive = ["You're a great person!", "Everyone likes you!", "Great things are coming your way!", "You're awesome!", "You make the world a better place to be!"];
+var negative = ["You're a terrible person!", "Everyone hates you!", "Bad things are headed your way!", "You're the opposite of awesome!", "You make the world a negative place to be!"];
 
+var randomP = positive[Math.floor(Math.random() * positive.length)];
+console.log("Positive: " + randomP);
+var randomN = negative[Math.floor(Math.random() * negative.length)];
+console.log("Negative: " + randomN);
 
 // Here we create a generic function to handle requests and responses
 function handleRequestOne(request, response) {
 
   // The below statement is triggered (client-side) when the user visits the PORT URL
-  response.end("It Works!! Path Hit: " + request.url);
+  response.end(randomP + request.url);
 }
 
 function handleRequestTwo(request, response) {
 
   // The below statement is triggered (client-side) when the user visits the PORT URL
-  response.end("It Works!! Path Hit: " + request.url);
+  response.end(randomN + request.url);
 }
 
 // Here we use the Node HTTP package to create our server.
